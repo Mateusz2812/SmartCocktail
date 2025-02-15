48cd764e74e816ad145632a431a20c19800e8a7f
Mateusz2812 <pawelekmateusz1@tlen.pl>
2025-02-15 18:35:11 +0100

    SmartCocktail - Firebase refactor.

--- app/src/main/java/com/smartCocktails/smartcocktail/WelcomeActivity.kt
+++ app/src/main/java/com/smartCocktails/smartcocktail/WelcomeActivity.kt
@@ -1,7 +1,12 @@
 package com.smartCocktails.smartcocktail
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Build
+import androidx.annotation.RequiresApi
 import androidx.compose.runtime.Composable
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.smartCocktails.core.base.BaseActivity
 import com.smartCocktails.core.navigator.AppInternalCodes
 import com.smartCocktails.core.navigator.InternalNavigator
@@ -14,13 +19,23 @@ class WelcomeActivity : BaseActivity() {
     @Inject
     lateinit var internalNavigator: InternalNavigator
 
-    override fun afterViews() {}
+    override fun afterViews() {
+        checkNotificationPermission()
+    }
 
     @Composable
     override fun ContentView() {
         WelcomeScreen(
             onClick = ::onClick
         )
+    }
+
+    private fun checkNotificationPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.POST_NOTIFICATIONS), 100)
+            }
+        }
     }
 
     private fun onClick() {
